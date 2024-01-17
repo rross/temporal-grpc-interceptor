8e8f2959dd5a2e5e2f33ba1a7cc9029ca78fd725
Rick Ross <rick.ross@temporal.io>
2024-01-17 17:38:14 -0500

    Clean up

--- src/main/java/io/temporal/demo/AppConfig.java
+++ src/main/java/io/temporal/demo/AppConfig.java
@@ -2,8 +2,8 @@ package io.temporal.demo;
 
 public final class AppConfig {
     public static final String TASK_QUEUE = "interceptor";
-    public static final String TARGET_NAMESPACE="rick-ross.a2dd6";
+    public static final String TARGET_NAMESPACE="<namespace>.<account>"; // Temporal Cloud format
 
-    public static final String TLS_CERT_PATH="/Users/rickross/dev/samples-server/tls/client-only/mac/rick-ross.pem";
-    public static final String TLS_KEY_PATH="/Users/rickross/dev/samples-server/tls/client-only/mac/rick-ross.key";
+    public static final String TLS_CERT_PATH="/path/to/cert.pem";
+    public static final String TLS_KEY_PATH="/path/to/cert.key";
 }
